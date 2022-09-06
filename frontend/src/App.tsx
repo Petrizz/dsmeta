986bbe0de20e53885e81a9dee5962a1ea716ac5b
Petrizz <pedrogonzagamendesoficial@gmail.com>
2022-09-05 21:52:29 -0300

    primeiro componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
-function App() { 
-    return(
+import NotificationButon from "./components/NotificationButton"
+
+function App() {
+  return (
+    <>
       <h1>Olá mundo</h1>
-    )
+      <NotificationButon></NotificationButon>
+    </>
+  )
 }
 
 export default App
